58c867578ea734dd77d0a4a52e2248d4d937816a
Summer-PC <1270102724@qq.com>
2020-10-21 10:33:25 +0800

    first commit

---
+++ demo1/src/test/java/demo1Test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class demo1Test {
+    @Test
+    public void fun01(){
+        Assertions.assertEquals(1,1);
+    }
+}
